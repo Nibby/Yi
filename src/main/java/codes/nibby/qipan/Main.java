bb3f9c0a94b3ba81c5571ef583b77d64d1390286
Nibby <nibby@null.net>
2019-08-23 03:14:18 +1200

    Established Gradle project structure

---
+++ src/main/java/codes/nibby/qipan/Main.java
@@ -0,0 +1,19 @@
+package codes.nibby.qipan;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(new BorderPane(), 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
