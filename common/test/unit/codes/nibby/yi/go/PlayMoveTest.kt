70a587114d6af3beec0f0bcc8ebbb16621bf2ed9
Kevin Yang <nibbyman@gmail.com>
2020-06-30 20:29:42 +1200

    Move PlayMoveTest.kt to unit test

--- common/test/unit/codes/nibby/yi/go/PlayMoveTest.kt
+++ common/test/unit/codes/nibby/yi/go/PlayMoveTest.kt
@@ -3,7 +3,6 @@ package codes.nibby.yi.go
 import codes.nibby.yi.go.rules.GoGameRulesHandler
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.time.Duration
 
 class PlayMoveTest {
 
