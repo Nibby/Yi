ef5502476fbd2d4a3dcb9ba36800e081c0261080
Nibby <nibby@null.net>
2020-06-30 20:27:41 +1200

    Remove performance time assertions from PlayMoveTest.kt
    Originally added to catch infinite while loops during
    the implementation of GoMoveHelper, but the assertions
     don't seem to be running on a separate thread...
    Meaning that it relies on the task to complete first
    before asserting performance time.
    
    A performance test suite will be added later. It's
    better for tests to fail for a single reason.

--- common/test/integration/codes/nibby/yi/go/PlayMoveTest.kt
+++ common/test/integration/codes/nibby/yi/go/PlayMoveTest.kt
@@ -71,179 +71,157 @@ class PlayMoveTest {
     fun `play first legal move has correct move tree state`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            val result = model.playMove(0, 0)
+        val result = model.playMove(0, 0)
 
-            Assertions.assertEquals(result.moveNode!!, model.currentNode)
-        }
+        Assertions.assertEquals(result.moveNode!!, model.currentNode)
     }
 
     @Test
     fun `play first legal move returns correct move submission result`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            val moveSubmitResult = model.playMove(0, 0)
+        val moveSubmitResult = model.playMove(0, 0)
 
-            Assertions.assertEquals(MoveValidationResult.OK, moveSubmitResult.validationResult)
-            Assertions.assertNotNull(moveSubmitResult.moveNode)
-            Assertions.assertTrue(moveSubmitResult.played)
-        }
+        Assertions.assertEquals(MoveValidationResult.OK, moveSubmitResult.validationResult)
+        Assertions.assertNotNull(moveSubmitResult.moveNode)
+        Assertions.assertTrue(moveSubmitResult.played)
     }
 
     @Test
     fun `play first legal move resolves to correct board position`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            val moveSubmitResult = model.playMove(0, 0)
-            val newNode = moveSubmitResult.moveNode
+        val moveSubmitResult = model.playMove(0, 0)
+        val newNode = moveSubmitResult.moveNode
 
-            val firstMoveGameState = model.resolveGameState(newNode!!)
-            val stoneColorAtPlayedMove = firstMoveGameState.gamePosition.getStoneColorAt(0, 0)
+        val firstMoveGameState = model.resolveGameState(newNode!!)
+        val stoneColorAtPlayedMove = firstMoveGameState.gamePosition.getStoneColorAt(0, 0)
 
-            Assertions.assertEquals(GoStoneColor.BLACK, stoneColorAtPlayedMove)
-        }
+        Assertions.assertEquals(GoStoneColor.BLACK, stoneColorAtPlayedMove)
     }
 
     @Test
     fun `play five consecutive moves all have correct stone color`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            val moveSubmitResult = model.playMove(0, 0)
-            val newNode = moveSubmitResult.moveNode
+        val moveSubmitResult = model.playMove(0, 0)
+        val newNode = moveSubmitResult.moveNode
 
-            val firstMoveGameState = model.resolveGameState(newNode!!)
-            val stoneColorAtPlayedMove = firstMoveGameState.gamePosition.getStoneColorAt(0, 0)
+        val firstMoveGameState = model.resolveGameState(newNode!!)
+        val stoneColorAtPlayedMove = firstMoveGameState.gamePosition.getStoneColorAt(0, 0)
 
-            Assertions.assertEquals(GoStoneColor.BLACK, stoneColorAtPlayedMove)
-        }
+        Assertions.assertEquals(GoStoneColor.BLACK, stoneColorAtPlayedMove)
     }
 
     @Test
     fun `one stone capture works`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            // Play black stone in the corner
-            model.playMove(0, 0)
+        // Play black stone in the corner
+        model.playMove(0, 0)
 
-            // Play two white stones to capture it
-            model.playMove(1, 0) // white removes a liberty
-            model.playPass()
-            model.playMove(0, 1) // white removes last liberty, stone at 0,0 should be captured now
+        // Play two white stones to capture it
+        model.playMove(1, 0) // white removes a liberty
+        model.playPass()
+        model.playMove(0, 1) // white removes last liberty, stone at 0,0 should be captured now
 
-            val gameState = model.resolveGameState(model.currentNode)
-            Assertions.assertEquals(1, gameState.prisonersWhite)
-            Assertions.assertEquals(GoStoneColor.NONE, gameState.gamePosition.getStoneColorAt(0, 0))
-        }
+        val gameState = model.resolveGameState(model.currentNode)
+        Assertions.assertEquals(1, gameState.prisonersWhite)
+        Assertions.assertEquals(GoStoneColor.NONE, gameState.gamePosition.getStoneColorAt(0, 0))
     }
 
     @Test
     fun `connected string capture works with primary moves`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            // Black plays three stones along the 1st column, white surrounds it on the 2nd column
-            model.playMove(0, 0)
-            model.playMove(1, 0)
-            model.playMove(0, 1)
-            model.playMove(1, 1)
-            model.playMove(0, 2)
-            model.playMove(1, 2) // After white plays this move, the three black stones on the left column should be all captured
+        // Black plays three stones along the 1st column, white surrounds it on the 2nd column
+        model.playMove(0, 0)
+        model.playMove(1, 0)
+        model.playMove(0, 1)
+        model.playMove(1, 1)
+        model.playMove(0, 2)
+        model.playMove(1, 2) // After white plays this move, the three black stones on the left column should be all captured
 
-            val currentState = model.resolveGameState(model.currentNode)
+        val currentState = model.resolveGameState(model.currentNode)
 
-            Assertions.assertEquals(3, currentState.prisonersWhite)
-            Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 0))
-            Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 1))
-            Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 2))
-            Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 0))
-            Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 1))
-            Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 2))
-        }
+        Assertions.assertEquals(3, currentState.prisonersWhite)
+        Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 0))
+        Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 1))
+        Assertions.assertEquals(GoStoneColor.NONE, currentState.gamePosition.getStoneColorAt(0, 2))
+        Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 0))
+        Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 1))
+        Assertions.assertEquals(GoStoneColor.WHITE, currentState.gamePosition.getStoneColorAt(1, 2))
     }
 
     @Test
     fun `game model state hash is correct after playing two legal moves`() {
         val model = GoGameModel(2, 2, TestingGameRulesNoSuicide(), TestingFourIntersectionXORHasher())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            model.playMove(0, 0)
-            model.playMove(1, 0)
+        model.playMove(0, 0)
+        model.playMove(1, 0)
 
-            val stateHashHistory = model.getStateHashHistory()
+        val stateHashHistory = model.getStateHashHistory()
 
-            Assertions.assertEquals(2, stateHashHistory.size)
-            Assertions.assertEquals(13, model.currentNode.data!!.stateHash) // 0100b xor 1001b = 1101b (13)
-            Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
-        }
+        Assertions.assertEquals(2, stateHashHistory.size)
+        Assertions.assertEquals(13, model.currentNode.data!!.stateHash) // 0100b xor 1001b = 1101b (13)
+        Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
     }
 
     @Test
     fun `game model state hash ignores pass moves`() {
         val model = GoGameModel(2, 2, TestingGameRulesNoSuicide(), TestingFourIntersectionXORHasher())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            model.playMove(0, 0)
-            model.playPass()
+        model.playMove(0, 0)
+        model.playPass()
 
-            val stateHashHistory = model.getStateHashHistory()
+        val stateHashHistory = model.getStateHashHistory()
 
-            Assertions.assertEquals(1, stateHashHistory.size)
-            Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
-        }
+        Assertions.assertEquals(1, stateHashHistory.size)
+        Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
     }
 
     @Test
     fun `game model state hash ignores resignation moves`() {
         val model = GoGameModel(2, 2, TestingGameRulesNoSuicide(), TestingFourIntersectionXORHasher())
 
-        Assertions.assertTimeout(Duration.ofMillis(2)) {
-            model.playMove(0, 0)
-            model.playResign()
+        model.playMove(0, 0)
+        model.playResign()
 
-            val stateHashHistory = model.getStateHashHistory()
+        val stateHashHistory = model.getStateHashHistory()
 
-            Assertions.assertEquals(1, stateHashHistory.size)
-            Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
-        }
+        Assertions.assertEquals(1, stateHashHistory.size)
+        Assertions.assertEquals(4, model.currentNode.parent!!.data!!.stateHash) // 0100b (4)
     }
 
     @Test
     fun `ko recapture is illegal`() {
         val model = GoGameModel(3, 3, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(40)) {
-            model.playMove(0, 0)
-            model.playMove(1, 0)
-            model.playMove(1, 1)
-            model.playMove(0, 1) // white captures black at 0,0
-            model.playMove(2, 0) // black sets up atari on 1,0 stone
-            model.playPass() // white passes so black can capture 1,0 by playing at 0,0
-            model.playMove(0, 0) // black captures and starts ko
+        model.playMove(0, 0)
+        model.playMove(1, 0)
+        model.playMove(1, 1)
+        model.playMove(0, 1) // white captures black at 0,0
+        model.playMove(2, 0) // black sets up atari on 1,0 stone
+        model.playPass() // white passes so black can capture 1,0 by playing at 0,0
+        model.playMove(0, 0) // black captures and starts ko
 
-            // This move should now be illegal because 1,0 was just captured
-            val submitResult = model.playMove(1, 0)
+        // This move should now be illegal because 1,0 was just captured
+        val submitResult = model.playMove(1, 0)
 
-            Assertions.assertEquals(MoveValidationResult.ERROR_KO_RECAPTURE, submitResult.validationResult)
-        }
+        Assertions.assertEquals(MoveValidationResult.ERROR_KO_RECAPTURE, submitResult.validationResult)
     }
 
     @Test
     fun `suicidal move not allowed on rules that prohibit suicide`() {
         val model = GoGameModel(2, 2, TestingGameRulesNoSuicide())
 
-        Assertions.assertTimeout(Duration.ofMillis(40)) {
-            model.playMove(1, 0)
-            model.playPass()
-            model.playMove(0, 1)
+        model.playMove(1, 0)
+        model.playPass()
+        model.playMove(0, 1)
 
-            val submitResult = model.playMove(0, 0) // Tries to play inside black territory (0 liberties)
+        val submitResult = model.playMove(0, 0) // Tries to play inside black territory (0 liberties)
 
-            Assertions.assertEquals(MoveValidationResult.ERROR_MOVE_SUICIDAL, submitResult.validationResult)
-        }
+        Assertions.assertEquals(MoveValidationResult.ERROR_MOVE_SUICIDAL, submitResult.validationResult)
     }
 
     @Test
