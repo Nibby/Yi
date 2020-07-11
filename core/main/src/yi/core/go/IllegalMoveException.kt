626d09207285cffe6140a51770e0c4a570c36f87
Nibby <nibby@null.net>
2020-07-12 02:14:01 +1200

    #32 Rename SubmitGoMoveException.kt -> IllegalMoveException.kt

--- core/main/src/yi/core/go/IllegalMoveException.kt
+++ core/main/src/yi/core/go/IllegalMoveException.kt
@@ -3,7 +3,7 @@ package yi.core.go
 /**
  * An exception thrown to represent a submitted move is illegal and cannot be played.
  */
-class SubmitGoMoveException : GoGameRulesException {
+class IllegalMoveException : GoGameRulesException {
     constructor(validationResult: GoMoveValidationResult, message: String?) : super(validationResult, message)
     constructor(validationResult: GoMoveValidationResult) : super(validationResult)
 }
