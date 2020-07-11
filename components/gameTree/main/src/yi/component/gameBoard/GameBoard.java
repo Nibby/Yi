0729b59b90764970f883ad4a06f41bb8506f430d
Kevin Yang <nibbyman@gmail.com>
2020-07-12 01:59:42 +1200

    #32 Finalize Gradle structure

---
+++ components/gameTree/main/src/yi/component/gameBoard/GameBoard.java
@@ -0,0 +1,11 @@
+package yi.component.gameBoard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameBoard extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
