e2a2a9fca7aeb02850162a7003140ae1ff4f12c6
Nibby <nibby@null.net>
2020-07-25 01:05:20 +1200

    #13 Rudimentary tree rendering

--- components/gameTree/src/main/java/yi/component/gametree/GameTreeCanvas.java
+++ components/gameTree/src/main/java/yi/component/gametree/GameTreeCanvas.java
@@ -3,6 +3,8 @@ package yi.component.gametree;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import yi.core.common.GameNode;
+import yi.core.go.GoGameStateUpdate;
 
 import java.util.Collection;
 
@@ -17,13 +19,41 @@ final class GameTreeCanvas extends Canvas {
         graphics = getGraphicsContext2D();
     }
 
-    public void render(Collection<TreeElement> visibleElements) {
+    public void render(Collection<TreeElement> visibleElements, GameNode<GoGameStateUpdate> currentNode) {
+        graphics.clearRect(0, 0, getWidth(), getHeight());
+        graphics.setStroke(Color.BLACK);
+        graphics.setLineWidth(2d);
         graphics.setFill(Color.BLACK);
+
+        for (var element : visibleElements) {
+            element.getParent().ifPresent(parent -> {
+                int px = parent.getLogicalX() * ELEMENT_WIDTH;
+                int py = parent.getLogicalY() * ELEMENT_HEIGHT;
+
+                double pCenterX = px + ELEMENT_WIDTH / 2d;
+                double pCenterY = py + ELEMENT_HEIGHT / 2d;
+
+                int x = element.getLogicalX() * ELEMENT_WIDTH;
+                int y = element.getLogicalY() * ELEMENT_HEIGHT;
+
+                double centerX = x + ELEMENT_WIDTH / 2d;
+                double centerY = y + ELEMENT_HEIGHT / 2d;
+
+                graphics.strokeLine(pCenterX, pCenterY, centerX, centerY);
+            });
+        }
 
         for (var element : visibleElements) {
             int x = element.getLogicalX() * ELEMENT_WIDTH;
             int y = element.getLogicalY() * ELEMENT_HEIGHT;
 
+            graphics.setFill(Color.BLACK);
+            if (element instanceof TreeNodeElement) {
+                var node = ((TreeNodeElement) element).getNode();
+                if (node.equals(currentNode)) {
+                    graphics.setFill(Color.BLUE);
+                }
+            }
             graphics.fillOval(x + 3, y + 3, ELEMENT_WIDTH - 6, ELEMENT_HEIGHT - 6);
         }
     }
