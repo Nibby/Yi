29aa80760d7a82cad2b224949c9ba65566e0d449
Kevin Yang <nibbyman@gmail.com>
2020-07-28 18:35:31 +1200

    Add some icon assets and test toolbar layout

--- editor/src/main/java/yi/editor/components/EditorMenuBar.java
+++ editor/src/main/java/yi/editor/components/EditorMenuBar.java
@@ -10,9 +10,13 @@ public class EditorMenuBar extends MenuBar {
 
     public EditorMenuBar() {
         var fileMenu = new Menu("File");
+        var editMenu = new Menu("Edit");
+        var toolsMenu = new Menu("Tools");
+        var viewMenu = new Menu("View");
+        var windowMenu = new Menu("Window");
         var helpMenu = new Menu("Help");
 
-        getMenus().addAll(fileMenu, helpMenu);
+        getMenus().addAll(fileMenu, editMenu, toolsMenu, viewMenu, windowMenu, helpMenu);
     }
 
 }
