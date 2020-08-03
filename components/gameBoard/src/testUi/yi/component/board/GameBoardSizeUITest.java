5fcad775f25d8f9dc23ac1e6790693c642793249
Nibby <nibby@null.net>
2020-08-03 20:19:08 +1200

    Remove GameBoardSizeUITest trial delay

--- components/gameBoard/src/testUi/yi/component/board/GameBoardSizeUITest.java
+++ components/gameBoard/src/testUi/yi/component/board/GameBoardSizeUITest.java
@@ -56,7 +56,6 @@ public class GameBoardSizeUITest extends GameBoardUITestBase {
                         assertSizeCorrect();
                     }
                 });
-                Thread.sleep(20);
             }
         }
     }
