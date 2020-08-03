d0e60d56c3925614295e0d03cdb409bc4471dba0
Nibby <nibby@null.net>
2020-08-03 23:17:01 +1200

    Refactor: Standardise naming. 'move' -> 'node'

--- core/src/main/kotlin/yi/core/go/MoveSequence.kt
+++ core/src/main/kotlin/yi/core/go/MoveSequence.kt
@@ -10,7 +10,7 @@ class MoveSequence constructor(private val game: GameModel) {
      * Otherwise a [GameRulesException] is thrown to interrupt the sequence flow.
      */
     fun playMove(x: Int, y:Int): MoveSequence {
-        val result = game.submitMove(x, y)
+        val result = game.submitNode(x, y)
         if (result.validationResult != MoveValidationResult.OK) {
             throw GameRulesException(result.validationResult, "Cannot play move ($x, $y) at node position ${game.getCurrentNode().moveNumber}: ${result.validationResult}")
         }
