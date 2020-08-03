9f91b92962c650c5f4d099542eebd34c830423d4
Nibby <nibby@null.net>
2020-08-03 20:46:18 +1200

    Rename playMove() -> submitMove() and all other similar methods

--- core/src/main/kotlin/yi/core/go/MoveSequence.kt
+++ core/src/main/kotlin/yi/core/go/MoveSequence.kt
@@ -6,11 +6,11 @@ package yi.core.go
 class MoveSequence constructor(private val game: GameModel) {
 
     /**
-     * Synonymous to [GameModel.playMove], but introduces an additional state check to verify the move is submitted successfully.
+     * Synonymous to [GameModel.submitMove], but introduces an additional state check to verify the move is submitted successfully.
      * Otherwise a [GameRulesException] is thrown to interrupt the sequence flow.
      */
     fun playMove(x: Int, y:Int): MoveSequence {
-        val result = game.playMove(x, y)
+        val result = game.submitMove(x, y)
         if (result.validationResult != MoveValidationResult.OK) {
             throw GameRulesException(result.validationResult, "Cannot play move ($x, $y) at node position ${game.getCurrentMove().moveNumber}: ${result.validationResult}")
         }
@@ -28,22 +28,22 @@ class MoveSequence constructor(private val game: GameModel) {
     }
 
     /**
-     * Synonymous to [GameModel.playPass].
+     * Synonymous to [GameModel.submitPass].
      *
-     * @see [GameModel.playPass]
+     * @see [GameModel.submitPass]
      */
     fun pass(): MoveSequence {
-        game.playPass()
+        game.submitPass()
         return this
     }
 
     /**
-     * Synonymous to [GameModel.playResign].
+     * Synonymous to [GameModel.submitResign].
      *
-     * @see [GameModel.playResign]
+     * @see [GameModel.submitResign]
      */
     fun resign(): MoveSequence {
-        game.playResign()
+        game.submitResign()
         return this
     }
 }
--- core/src/test/unit/yi/core/go/GameModelEventsTest.kt
+++ core/src/test/unit/yi/core/go/GameModelEventsTest.kt
@@ -65,7 +65,7 @@ class GameModelEventsTest {
         // Method under test
         model.onNodeRemove().addListener(eventListener)
 
-        val submitResult = model.playMove(0, 0)
+        val submitResult = model.submitMove(0, 0)
         model.removeNodeSubtree(submitResult.moveNode!!) // Has to be a legal move
         Thread.sleep(10)
         Assertions.assertTrue(eventReceived, "No event emitted")
@@ -91,7 +91,7 @@ class GameModelEventsTest {
                 .playMove(0, 0)
                 .playMove(1, 0)
 
-        val thirdMove = model.playMove(2, 0)
+        val thirdMove = model.submitMove(2, 0)
 
         // Sanity check
         Assertions.assertTrue(thirdMove.validationResult == MoveValidationResult.OK)
