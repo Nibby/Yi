202552af560dd751b2f0f9dcd84bf21c7b43344d
Kevin Yang <nibbyman@gmail.com>
2020-08-15 14:20:22 +1200

    Work in progress #23 save/load framework

---
+++ core/src/main/kotlin/yi/core/go/GameModelExporter.java
@@ -0,0 +1,7 @@
+package yi.core.go;
+
+// This class is written in Java because it is a utility class, and
+// Kotlin interop will force usage of .Companion or append Kt as type suffix.
+
+public final class GameModelExporter {
+}
