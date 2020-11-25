375dada81707d168164e194c243ef52484d8a299
Nibby <nibby@null.net>
2020-11-26 03:16:00 +1300

    Re-design editor toolbar aesthetics and layout
    * Also re-styled icons to match new toolbar design

--- components/gameBoard/src/main/java/yi/component/board/GameBoardView.java
+++ components/gameBoard/src/main/java/yi/component/board/GameBoardView.java
@@ -2,6 +2,7 @@ package yi.component.board;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import org.jetbrains.annotations.Nullable;
 
 final class GameBoardView {
 
@@ -9,7 +10,7 @@ final class GameBoardView {
 
     public CoordinateLabelPosition coordinateLabelPosition = CoordinateLabelPosition.getDefaultValue();
     public PresetStoneStyle presetStoneStyle = PresetStoneStyle.getDefaultValue();
-    public Image boardImage = new Image(this.getClass().getResourceAsStream("/defaultBoard.png"));
-    public Image backgroundImage = new Image(this.getClass().getResourceAsStream("/defaultBackground.jpg"));
+    public @Nullable Image boardImage = new Image(this.getClass().getResourceAsStream("/defaultBoard.png"));
+    public @Nullable Image backgroundImage = new Image(this.getClass().getResourceAsStream("/defaultBackground.jpg"));
     public Color boardGridColor = new Color(158d/255d, 103d/255d, 35d/255d, 1.0d);
 }
