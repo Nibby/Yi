c399735f339d4eb136faf682650c8ac1f0268e71
Nibby <nibby@null.net>
2020-11-26 12:49:11 +1300

    Add javadoc

--- components/common/src/main/java/yi/component/utilities/KeyModifier.java
+++ components/common/src/main/java/yi/component/utilities/KeyModifier.java
@@ -1,5 +1,14 @@
 package yi.component.utilities;
 
+import javafx.scene.input.KeyCode;
+
+/**
+ * List of available key combination modifiers for {@link GuiUtilities#getKeyCombination(KeyCode, KeyModifier...)}.
+ * <p/>
+ * Note use in meta/control modifier key between macOS and non-macOS platforms. In
+ * situations where the meta/control modifier should vary between platform, use the
+ * {@link #SHORTCUT} value instead.
+ */
 public enum KeyModifier {
     SHORTCUT,
     CTRL,
