9216a23fa7a9c01ba4b1e0af6a1c9866438df95a
Kevin Yang <nibbyman@gmail.com>
2020-11-27 00:05:48 +1300

    Clean up accelerator framework

---
+++ editor/src/main/java/yi/editor/components/EditorToolBar.java
@@ -0,0 +1,12 @@
+package yi.editor.components;
+
+import javafx.scene.control.ToolBar;
+
+public class EditorToolBar extends ToolBar {
+
+    public EditorToolBar() {
+        setVisible(false);
+        setManaged(false);
+    }
+
+}
