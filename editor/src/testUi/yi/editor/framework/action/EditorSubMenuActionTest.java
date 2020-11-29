ac4e7ef9fd472f84b8d1d228a85afa12b162d17e
Nibby <nibby@null.net>
2020-11-29 21:56:39 +1300

    Add better test failure messages

--- editor/src/testUi/yi/editor/framework/action/EditorSubMenuActionTest.java
+++ editor/src/testUi/yi/editor/framework/action/EditorSubMenuActionTest.java
@@ -56,22 +56,35 @@ public class EditorSubMenuActionTest extends EditorUITestBase {
         Menu testingMenu = getMenu(EditorMainMenuType.TESTING).orElseThrow();
 
         ObservableList<MenuItem> layer1Items = testingMenu.getItems();
-        Assertions.assertEquals(1, layer1Items.size());
-        Assertions.assertEquals(subMenu1.getAsMenuItem(), layer1Items.get(0));
+        Assertions.assertEquals(1, layer1Items.size(),
+                "Only subMenu1 should appear in first layer, got " + layer1Items.size() + " items");
+        Assertions.assertEquals(subMenu1.getAsMenuItem(), layer1Items.get(0),
+                "The only item in first layer is not subMenu1's component " +
+                        "(or at least .equals() returned false)");
 
         MenuItem theOnlyLayer1Item = layer1Items.get(0);
-        Assertions.assertTrue(theOnlyLayer1Item instanceof Menu);
+        Assertions.assertTrue(theOnlyLayer1Item instanceof Menu,
+                "subMenu1 exported a menu component that is not of type Menu");
 
         ObservableList<MenuItem> layer2Items = ((Menu) theOnlyLayer1Item).getItems();
-        Assertions.assertEquals(2, layer2Items.size());
-        Assertions.assertEquals(itemInSubMenu1.getAsMenuItem(), layer2Items.get(0));
-        Assertions.assertEquals(subMenu2.getAsMenuItem(), layer2Items.get(1));
+        Assertions.assertEquals(2, layer2Items.size(),
+                "itemInSubMenu1 and subMenu2 appears in layer 2, in that order. " +
+                        "Actual number of menu items in this layer is " + layer2Items.size());
+        Assertions.assertEquals(itemInSubMenu1.getAsMenuItem(), layer2Items.get(0),
+                "First item in layer2 is not itemInSubMenu1 (is it ordered correctly?)");
+        Assertions.assertEquals(subMenu2.getAsMenuItem(), layer2Items.get(1),
+                "Second item in layer2 is not subMenu2  (is it ordered correctly?)");
 
         MenuItem layer2SubMenu = layer2Items.get(1);
-        Assertions.assertTrue(layer2SubMenu instanceof Menu);
+        Assertions.assertTrue(layer2SubMenu instanceof Menu,
+                "Second item in layer 2 is not an instance of Menu");
 
         ObservableList<MenuItem> layer3Items = ((Menu) layer2SubMenu).getItems();
-        Assertions.assertEquals(1, layer3Items.size());
-        Assertions.assertEquals(itemInSubMenu2.getAsMenuItem(), layer3Items.get(0));
+        Assertions.assertEquals(1, layer3Items.size(),
+                "Only itemInSubMenu2 should appear in layer 3, got " + layer3Items.size()
+                        + " components");
+        Assertions.assertEquals(itemInSubMenu2.getAsMenuItem(), layer3Items.get(0),
+                "Item in layer 3 is not the same item exported by itemInSubMenu2, " +
+                        "or at least equals() returned false.");
     }
 }
