55bd3e76c83df6f64131bd1fdae1947cad03131b
Kevin Yang <nibbyman@gmail.com>
2020-12-29 14:12:29 +1300

    Add missing javadoc

--- yi.editor/src/main/java/yi/editor/framework/EditorHelper.java
+++ yi.editor/src/main/java/yi/editor/framework/EditorHelper.java
@@ -1,7 +1,6 @@
 package yi.editor.framework;
 
 import javafx.scene.text.Font;
-import org.jetbrains.annotations.Nullable;
 import yi.component.shared.audio.CommonAudioSets;
 import yi.component.shared.audio.SoundManager;
 import yi.component.shared.component.FontManager;
@@ -16,10 +15,12 @@ import yi.editor.settings.EditorSettings;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 
+/**
+ * Global helper class that manages various startup configurations for the editor
+ * application.
+ */
 public final class EditorHelper {
 
     private static final Version VERSION = new Version(0, 0, 1, "alpha");
@@ -27,52 +28,83 @@ public final class EditorHelper {
 
     private static boolean useSystemMenuBar = SystemUtilities.isMac();
     private static boolean runningAsTest = false;
-    private static Path preferredSettingsRootPath = null;
 
     private EditorHelper() {
         // Utility class, no instantiation
     }
 
+    /**
+     * @return Version of this editor application.
+     */
     public static Version getVersion() {
         return VERSION;
     }
 
+    /**
+     * @return Name of this editor application.
+     */
     public static String getProgramName() {
         return PROGRAM_NAME;
     }
 
+    /**
+     * @return {@code true} if the application is currently run within an IDE rather than
+     * a distribution.
+     */
     public static boolean isRunningFromSource() {
         return System.getProperty("yi.fromSource", "false").equalsIgnoreCase("true");
     }
 
+    /**
+     * In debug mode, components are expected to display additional technical information
+     * reserved for development purposes.
+     *
+     * @return {@code true} if debug mode is to be enabled.
+     */
     public static boolean isDebugMode() {
         return !isRunningFromSource();
     }
 
+    /**
+     * @param isRunningAsTest {@code true} if the application is being run as part of a
+     * test suite.
+     */
     public static synchronized void setRunningAsTest(boolean isRunningAsTest) {
         EditorHelper.runningAsTest = isRunningAsTest;
     }
 
+    /**
+     * @return {@code true} if the application is being run as part of a test suite.
+     */
     public static boolean isRunningAsTest() {
         return runningAsTest;
     }
 
-    public static synchronized void setPreferredSettingsRootPath(@Nullable Path path) {
-        EditorHelper.preferredSettingsRootPath = path;
-    }
-
-    public static Optional<Path> getPreferredSettingsRootPath() {
-        return Optional.ofNullable(preferredSettingsRootPath);
-    }
-
+    /**
+     * Set whether the application menu bar should be displayed in a manner that appears
+     * native to the current operating system. For example on macOS, if this is enabled,
+     * the menu bar will appear on the top of the screen rather than a custom JavaFx
+     * component in the window itself.
+     *
+     * @param yes Whether to use system native menu bar appearance.
+     */
     public static void setUseSystemMenuBar(boolean yes) {
         useSystemMenuBar = yes;
     }
 
+    /**
+     * @return {@code true} if using system native menu bar appearance.
+     */
     public static boolean isUsingSystemMenuBar() {
         return useSystemMenuBar;
     }
 
+    /**
+     * Initializes various core components as part of the startup procedure, before
+     * any {@link yi.editor.EditorWindow} is created.
+     *
+     * This step should only be performed once upon startup.
+     */
     public static void initializeContext() {
         loadBundledFonts();
         YiScene.addExtraStylesheet("/yi/editor/fonts/font.css", EditorHelper.class);
